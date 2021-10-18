5f4cc6d8908e15ba68f0a8ed9100274a85f7f82c
flbusom <73123182+flbusom@users.noreply.github.com>
2021-10-18 18:52:21 +0200

    cambios

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TiendaOnline3
+# Tienda Online
+
+## A continuacion vereis la nueva tienda online de componentes para PC y mucho más.
+
+## Con los productos más innovadores del mercado y al mejor precio.
--- index.php
+++ index.php
@@ -20,30 +20,38 @@ if(!isset($_COOKIE["userCookie"]) && (!isset($_COOKIE["userToken"]))){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
-    <!-- <link rel="stylesheet" href="css/bootstrap431/css/bootstrap.min.css"> 
-    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/all.min.css"> -->
+    <title>Pagina Inicio</title>
+
+    <link rel="stylesheet" href="css/bootstrap431/css/bootstrap.min.css"> 
+    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 
 <header >
-    <div class="header_left">
+
+    <!-- <div class="header_left">
        <h1><b><?php echo htmlspecialchars($user_id); ?></b></h1>   
-   </div>
-   <div class="header_right">
-    <h1><b><?php echo htmlspecialchars($user_id); ?></b></h1>
-</div>
+   </div>  -->
+
+    <div class="header_right">
+        <h1><b><?php echo htmlspecialchars($user_id); ?></b></h1>
+    </div>
 
 </header>
 <body>
 
-    <p>
+    <p id = "btncerrar">
       
         <a href="logout.php" class="btn btn-danger">Cerrar sesión</a>
     </p>
 
+   <!--  <p id="btncerrar">
+        <button id="btn-salirsession"><a style="text-decoration:none" href="logout.php" class="btn btn-danger">Salir de la session</a></button>
+    </p> -->
 
 
 </body>
