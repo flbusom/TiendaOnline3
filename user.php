efe0c07376466f173ab93ce134d4a6678235b07c
flbusom <73123182+flbusom@users.noreply.github.com>
2021-10-22 19:39:24 +0200

    usuario

--- user.php
+++ user.php
@@ -1,8 +1,6 @@
 <?php
 
 
-
-
 ?>
 
 
@@ -13,20 +11,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styleuser.css">
     <title>Pagina Usuario</title>
 </head>
 <body>
 
-<h1>hola</h1>
 
 
-
-<div id = "btncerrar">
-      
-      <a href="logout.php" class="btn btn-danger">Cerrar sesión</a>
-  </div> 
-
-
+    <nav><ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Carrito</a></li>
+            <li><a href="logout.php" class="btn btn-danger">Cerrar sesión</a></li>
+        </ul></nav>
 
 
 
